b293447652a1bce4108338a0bf55cf94a9ef3fee
Michael <michaeljoseph6950@gmail.com>
2022-08-23 08:34:30 -0700

    ch

--- js/main/main.js
+++ js/main/main.js
@@ -13,19 +13,6 @@
 
         }
     });
-    $window.on('scroll', function() {
-        if ($(this).scrollTop() > 500) {
-            $(".scroll-to-top").fadeIn(400);
-        } else {
-            $(".scroll-to-top").fadeOut(400);
-        }
-    });
-    $(".scroll-to-top").on('click', function(event) {
-        event.preventDefault();
-        $("html, body").animate({
-            scrollTop: 0
-        }, 600);
-    });
 
     var pageSection = $(".parallax,.bg-img");
     pageSection.each(function(indx) {
@@ -34,143 +21,6 @@
         }
     });
 
-    $(document).ready(function() {
-
-
-        $('#services-carousel').owlCarousel({
-            loop: true,
-            responsiveClass: true,
-            dots: true,
-            nav: true,
-            smartSpeed: 500,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            autoplayHoverPause: true,
-            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-            responsive: {
-                0: {
-                    items: 2,
-                    margin: 10
-                },
-                768: {
-                    items: 3,
-                    margin: 10
-                },
-                992: {
-                    items: 3,
-                    margin: 10
-                },
-                1200: {
-                    items: 5,
-                    margin: 10
-                }
-            }
-        });
-
-        if ($("#rev_slider_2").length !== 0) {
-			var tpj = jQuery;
-			var revapi2;
-			tpj(document).ready(function() {
-				if (tpj("#rev_slider_2").revolution == undefined) {
-					revslider_showDoubleJqueryError("#rev_slider_2");
-				} else {
-					revapi2 = tpj("#rev_slider_2").show().revolution({
-                        sliderLayout: "fullscreen",
-                        delay: 12e3,
-                        responsiveLevels: [4096, 1024, 778, 420],
-                        gridwidth: [1370, 1024, 778, 410],
-                        gridheight: 600,
-                        hideThumbs: 10,
-                        fullScreenAutoWidth: "on",
-                        dots: true,
-                        fullScreenOffsetContainer: ".rev-offset",
-                        navigation: {
-                            onHoverStop: "off",
-                            touch: {
-                                touchenabled: "on",
-                                swipe_threshold: 75,
-                                swipe_min_touches: 1,
-                                swipe_direction: "horizontal",
-                                drag_block_vertical: !1
-                            },
-                            arrows: {
-                                enable: !0,
-                                style: "hermes",
-                                tmp: '<div class="tp-arr-allwrapper">  <div class="tp-arr-imgholder"></div> <div class="tp-arr-titleholder">{{title}}</div> </div>',
-                                left: {
-                                    h_align: "left",
-                                    v_align: "center",
-                                    h_offset: 0,
-                                    v_offset: 0
-                                },
-                                right: {
-                                    h_align: "right",
-                                    v_align: "center",
-                                    h_offset: 0,
-                                    v_offset: 0
-                                },
-                                hide_onleave: false,
-								hide_onmobile: false,
-                            },
-
-                            // arrows: {
-                            //     enable: !0,
-                            //     style: "metis",
-                            //     tmp: "",
-                            //     rtl: !1,
-                            //     hide_onleave: false,
-							// 	hide_onmobile: false,
-                            //     hide_under: 0,
-                            //     hide_over: 9999,
-                            //     hide_delay: 200,
-                            //     hide_delay_mobile: 1200,
-                            //     left: {
-                            //         container: "slider",
-                            //         h_align: "left",
-                            //         v_align: "center",
-                            //         h_offset: 20,
-                            //         v_offset: 0
-                            //     },
-                            //     right: {
-                            //         container: "slider",
-                            //         h_align: "right",
-                            //         v_align: "center",
-                            //         h_offset: 20,
-                            //         v_offset: 0
-                            //     }
-                            // },
-
-
-                            bullets: {
-                                style: "",
-                                style: "hesperiden",
-                                enable: 1,
-                                hide_onmobile: false,
-                                hide_onleave: false,
-                                direction: "horizontal",
-                                space: 12,
-                                h_align: "center",
-                                v_align: "bottom",
-                                h_offset: 0,
-                                v_offset: 30,
-                                tmp: ""
-                            }
-                        },
-                        parallax: {
-                            type: "scroll",
-                            levels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85],
-                            origo: "enterpoint",
-                            speed: 400,
-                            bgparallax: "on",
-                            disable_onmobile: "on"
-                        },
-                        spinner: "spinner4"
-					});
-				}
-			});
-		}
-
-    });
     $window.on("load", function() {
         $.scrollIt({
             upKey: 38,
@@ -258,7 +108,6 @@ $('#exampleModal').on('show.bs.modal', function (event) {
 
     function addToCartClicked(event){
         var btn = event.target;
-        // alert(btn.id);
         var price = btn.innerText;
         var image = btn.parentElement.parentElement.children[0].children[1].src;
         var balance = btn.parentElement.parentElement.children[0].children[0].innerText;
@@ -270,6 +119,7 @@ $('#exampleModal').on('show.bs.modal', function (event) {
         var info5 = btn.parentElement.children[5].innerText;
         var info6 = btn.parentElement.children[6].innerText;
         var account = btn.parentElement.children[7].innerText;
+        var theid = btn.id;
         
         modal.find(".modal-title").text("Balance: " + balance);
         modal.find(".money").text(price);
@@ -280,8 +130,9 @@ $('#exampleModal').on('show.bs.modal', function (event) {
         modal.find(".info4 p").text(info4);
         modal.find(".info5 p").text(info5);
         modal.find(".info6 p").text(info6);
-        modal.find(".image p").text(image);
+        modal.find(".modal-img").attr("src",image);
         modal.find(".account p").text(account);
+        modal.find(".theid p").text(theid);
         
     }
 })
