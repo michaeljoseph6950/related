c09f7c3eacb616bcf97d0e32842aeb3590e3fb48
Michael <michaeljoseph6950@gmail.com>
2022-08-23 17:08:55 -0700

    ch

--- js/cart/invoice.js
+++ js/cart/invoice.js
@@ -16,7 +16,7 @@ function move(){
                         alert("Time's up, select another bank log and pay for it before 10 minutes end!");
                         window.location.reload();
                     } 
-                    else if( width <= 200) {
+                    else if( width <= 2000) {
                         elemj.classList.add("bg-danger");
                         width--;
                         localStorage.setItem('time-left',width--);
@@ -28,7 +28,7 @@ function move(){
                         elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     } 
-                    else if( width <= 400) {
+                    else if( width <= 4000) {
                         elemj.classList.add("bg-warning");
                         width--;
                         localStorage.setItem('time-left',width--);
@@ -54,7 +54,7 @@ function move(){
                 }
             } else if(!localStorage.getItem('loadedtime')) {
                 var elemj = document.getElementById('pablos');
-                localStorage.setItem('time-left',600)
+                localStorage.setItem('time-left',6000)
                 var width = localStorage.getItem('time-left');
                 var id = setInterval(frame, 1000);
                 function frame(){
@@ -66,7 +66,7 @@ function move(){
                         alert("Time's up, select another bank log and pay for it before 10 minutes end!");
                         window.location.reload();
                     } 
-                    else if( width <= 200) {
+                    else if( width <= 2000) {
                         elemj.classList.add("bg-danger");
                         width--;
                         localStorage.setItem('time-left',width--);
@@ -78,7 +78,7 @@ function move(){
                         elemj.style.width = (width/6) + "%";
                         document.getElementById('escoz').innerHTML = `Time left: ${minutes}:${seconds}`;
                     } 
-                    else if( width <= 400) {
+                    else if( width <= 4000) {
                         elemj.classList.add("bg-warning");
                         width--;
                         localStorage.setItem('time-left',width--);
